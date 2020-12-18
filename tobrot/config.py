78c5e0212ccb737d23e5d0c8c5cddc0a531972d8
beyanchan <75328415+beyanchan@users.noreply.github.com>
2020-12-19 02:03:13 +0800

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,18 +1,18 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-    TG_BOT_TOKEN = ""
-    APP_ID = 
-    API_HASH = ""
-    AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, 715779594, 1161316820, 1291911714]
-    INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/0:/Trash"
-    YTDL_COMMAND = "ytdl@URL_Uploader_Max_Bot"
-    LEECH_COMMAND = "leech@URL_Uploader_Max_Bot"
+    TG_BOT_TOKEN = "1461818373:AAGsUoCxqoWZd_zB1AiCmGXQRP1X8rFOr0I"
+    APP_ID = 2105857
+    API_HASH = "c8aacc9417551dc98e2b2f61fbfb8387"
+    AUTH_CHANNEL = [-1001463255443]
+    INDEX_LINK = "https://nando.xyz/0:/"
+    YTDL_COMMAND = "ytdl"
+    LEECH_COMMAND = "leech"
     CLEAR_THUMBNAIL = "clearthumbnail"
     SAVE_THUMBNAIL = "savethumbnail"
-    OWNER_ID = 715779594
-    RCLONE_CONFIG = """"""
+    OWNER_ID = 1323460577
+    RCLONE_CONFIG = """{"access_token":"ya29.a0AfH6SMCA97PSbVnmQGcDS_JhHXcOw0CySDGxjzZyWjmrgWHDf9A1n-Rmdg4sVnUDxV9Y5_5tAdbhfpW6_xbF1O9ALAeBj1xmKFQtMubDyGfYB_i1LltJA_e_zJawP-peIK4yzoYrZTvdfP02U2PHfqHiV6WmX5RCUPmXOqHlMG4","token_type":"Bearer","refresh_token":"1//0guQVZQz65RjnCgYIARAAGBASNwF-L9IrWy7b2vaAF3Qyh-UjCszb0xVSG83KYCi4X9taiSfr_9EhDO13oqWNTuoVGris4xENImo","expiry":"2020-12-03T13:41:27.382397538Z"}"""
     DESTINATION_FOLDER = "Trash"
-    GLEECH_COMMAND = "gleech@URL_Uploader_Max_Bot"
-    TELEGRAM_LEECH_COMMAND_G = "tleech@URL_Uploader_Max_Bot"
-    PYTDL_COMMAND_G = "pytdl@URL_Uploader_Max_Bot"
+    GLEECH_COMMAND = "gleech"
+    TELEGRAM_LEECH_COMMAND_G = "tleech"
+    PYTDL_COMMAND_G = "pytdl"
